d29a64e3c85f8546be95ba7837039e5369a3d58d
AnimeshSri1405 <75259179+AnimeshSri1405@users.noreply.github.com>
2021-02-03 14:47:26 +0530

    added files

---
+++ sketch.js
@@ -0,0 +1,70 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+
+function setup() {
+  createCanvas(900,400);
+  engine = Engine.create();
+  world = engine.world;
+ 
+  ground = new Ground();
+  base1 = new Stand(390,300,250,10);
+  base2 = new Stand(700,200,200,10);
+ 
+  //level one
+  block1 = new Block(300,275,30,40);
+  block2 = new Block(330,275,30,40);
+  block3 = new Block(360,275,30,40);
+  block4 = new Block(390,275,30,40);
+  block5 = new Block(420,275,30,40);
+  block6 = new Block(450,275,30,40);
+  block7 = new Block(480,275,30,40);
+  block8 = new Block(330,235,30,40);
+  block9 = new Block(360,235,30,40);
+  block10 = new Block(390,235,30,40);
+  block11 = new Block(420,235,30,40);
+  block12 = new Block(450,235,30,40);
+  block13 = new Block(360,195,30,40);
+  block14 = new Block(390,195,30,40);
+  block15 = new Block(420,195,30,40);
+  block16 = new Block(390,155,30,40);
+
+  
+
+ 
+
+
+  Engine.run(engine);
+}
+function draw() {
+  background("white"); 
+ 
+ 
+  ground.display();
+  base1.display();
+  base2.display();
+  fill("blue");
+  block1.display();
+  block2.display();
+  block3.display();
+  block4.display();
+  block5.display();
+  block6.display();
+  block7.display();
+  fill("pink");
+  block8.display();
+  block9.display();
+  block10.display();
+  block11.display();
+  block12.display();
+  fill("yellow");
+  block13.display();
+  block14.display();
+  block15.display();
+  fill("grey");
+  block16.display();
+  fill("skyblue");
+  
+}
